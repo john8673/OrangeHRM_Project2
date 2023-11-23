7d27404c7374517533439339c78f02b9af51ece1
john8673 <mailstoabraham@gmail.com>
2023-11-23 22:33:01 +0530

    Increased wait time of browser driver

--- Test_Classes/Admin_Page_Test_Class.py
+++ Test_Classes/Admin_Page_Test_Class.py
@@ -14,7 +14,7 @@ class AdminPageTestCase(unittest.TestCase):
     def setUp(self) -> None:
         self.chrome_driver = webdriver.Chrome()
         self.chrome_driver.maximize_window()
-        self.chrome_driver.implicitly_wait(20)
+        self.chrome_driver.implicitly_wait(30)
         self.chrome_driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index")
         lp_object = Login_Page(self.chrome_driver)
         lp_object.enter_username()
@@ -24,11 +24,13 @@ class AdminPageTestCase(unittest.TestCase):
         ap_object.admin_page_click()
 
     # testcase to validate the title of the current window
+
     def test_admin_page_title(self):
         ap_object = Admin_Page(self.chrome_driver)
         assert ap_object.admin_page_window_title()
 
     # From line 31 to line 57, testcases to validate dropdown elements present in Admin page Header
+
     def test_is_user_management_dropdown_present(self):
         ap_object = Admin_Page(self.chrome_driver)
         assert ap_object.user_management_dropdown_validation()
